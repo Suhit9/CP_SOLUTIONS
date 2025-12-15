4e3588b6b7643289793b370b2c0b9e6e152663bf
Suhit Khandelwal <suhitkhandelwal9@gmail.com>
2025-12-15 08:39:11 +0530

    I Have Made Some Changes In It

--- Largest_Area_Histogram.cpp
+++ Largest_Area_Histogram.cpp
@@ -8,8 +8,6 @@ vector<int> rightSmallerEle(vector<int>&heights){
         while(!st.empty() && heights[st.top()]>=heights[i]){
             st.pop();
         }
-        
-        
          if(!st.empty()){
             ans[i]=st.top();
            
@@ -27,7 +25,6 @@ vector<int> leftSmallerEle(vector<int>&heights){
         while(!st.empty() && heights[st.top()]>=heights[i]){
             st.pop();
         }
-       
         if(!st.empty()){
             ans[i]=st.top();
            
